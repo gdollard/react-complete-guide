ffc6aa2be7e575af33b8fc2570936d424f66453f
Glenn Dollard <glenn.dollard@gmail.com>
2020-06-05 22:54:01 +0100

    update code to introduce react hooks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import React, { Component } from 'react';
-
+import React, { useState } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-class App extends Component {
-  render() {
+const app = props => {
+  // Hooks
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 27},
+      {name: 'Stephanie', age: 29}
+    ],
+    cars: {
+      amount: 3,
+      value: 1600
+    }
+  });
+
+  //yes, a function within a function
+  const switchNameHandler = () => {
+    setPersonsState( 
+      {
+        persons:  [
+          {name: 'Pip', age: 128},
+          {name: 'Elk', age: 227},
+          {name: 'Zorb', age: 2229}
+        ]
+    });
+  };
+
     return (
       <div className="App">
         <h1>HI, I'm a React app</h1>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name = {personsState.persons[0].name} age = {personsState.persons[0].age}/>
+        <Person name = {personsState.persons[1].name} age = {personsState.persons[1].age}>My hobbies are: Racing </Person>
+        <Person name = {personsState.persons[2].name} age = {personsState.persons[2].age}/>
       </div>
     );
-  }
 }
 
-export default App;
+export default app;
+
+
--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// render our root App component
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
