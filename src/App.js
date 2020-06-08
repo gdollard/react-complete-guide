f71a1dbf554cb37235379d434751d690ff738e55
Glenn <glenn.dollard@gmail.com>
2020-06-08 21:21:59 +0100

    assignment 1 solution

--- src/App.js
+++ src/App.js
@@ -1,44 +1,42 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person';
 
-const app = props => {
-  // Hooks
-  const [ personsState, setPersonsState ] = useState({
-    persons: [
-      {name: 'Max', age: 28},
-      {name: 'Manu', age: 27},
-      {name: 'Stephanie', age: 29}
-    ],
-    cars: {
-      amount: 3,
-      value: 1600
-    }
-  });
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
 
-  //yes, a function within a function
-  const switchNameHandler = () => {
-    setPersonsState( 
-      {
-        persons:  [
-          {name: 'Pip', age: 128},
-          {name: 'Elk', age: 227},
-          {name: 'Zorb', age: 2229}
-        ]
-    });
-  };
+class App extends Component {
+  state = {
+    username: 'supermax'
+  }
 
+  usernameChangedHandler = (event) => {
+    this.setState({username: event.target.value});
+  }
+
+  render() {
     return (
       <div className="App">
-        <h1>HI, I'm a React app</h1>
-        <button onClick={switchNameHandler}>Switch Name</button>
-        <Person name = {personsState.persons[0].name} age = {personsState.persons[0].age}/>
-        <Person name = {personsState.persons[1].name} age = {personsState.persons[1].age}>My hobbies are: Racing </Person>
-        <Person name = {personsState.persons[2].name} age = {personsState.persons[2].age}/>
+        <ol>
+          <li>Create TWO new components: UserInput and UserOutput</li>
+          <li>UserInput should hold an input element, UserOutput two paragraphs</li>
+          <li>Output multiple UserOutput components in the App component (any paragraph texts of your choice)</li>
+          <li>Pass a username (of your choice) to UserOutput via props and display it there</li>
+          <li>Add state to the App component (=> the username) and pass the username to the UserOutput component</li>
+          <li>Add a method to manipulate the state (=> an event-handler method)</li>
+          <li>Pass the event-handler method reference to the UserInput component and bind it to the input-change event</li>
+          <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
+          <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
+          <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
+        </ol>
+        <UserInput 
+          changed={this.usernameChangedHandler} 
+          currentName={this.state.username} />
+        <UserOutput userName={this.state.username} />
+        <UserOutput userName={this.state.username} />
+        <UserOutput userName="Max" />
       </div>
     );
+  }
 }
 
-export default app;
-
-
+export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-// render our root App component
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
