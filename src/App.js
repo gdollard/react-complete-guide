173596b2716a2ec761658c80eb07deb0169530de
Glenn <glenn.dollard@gmail.com>
2020-06-10 19:58:15 +0100

    latest for section 4

--- src/App.js
+++ src/App.js
@@ -1,39 +1,96 @@
 import React, { Component } from 'react';
 import './App.css';
-
-import UserInput from './UserInput/UserInput';
-import UserOutput from './UserOutput/UserOutput';
+import Person from './Person/Person';
 
 class App extends Component {
   state = {
-    username: 'supermax'
+    persons: [
+      { id: '1q70ew', name: 'Max', age: 28 },
+      { id: 'qqe545gbbw', name: 'Manu', age: 29 },
+      { id: 'qqeqa545w', name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
   }
 
-  usernameChangedHandler = (event) => {
-    this.setState({username: event.target.value});
+  deletePersonHandler = (personIndex) => {
+    //make a copy of the array using the spread operator (to avoid using the same state reference)
+    const persons = [...this.state.persons];
+    // remove 1 element from the array
+    persons.splice(personIndex, 1);
+    // now update the state with the modified array
+    this.setState({persons: persons});
+  };
+
+
+  nameChangedHandler = (event, id ) => {
+
+    // find the person we want to update, use findIndex on the array to iterate thru each element and compare ID 
+    const personIndex  = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    // Don't mutate the state directly so create a new instance of the person object
+    const person = {
+      // use the spread operator to extract all properties of the object and create a copy of it
+      ...this.state.persons[personIndex] 
+    }   
+
+    // now update the name of the person from the event object
+    person.name = event.target.value;
+
+    // now update the array, first get a copy of the array from state
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {
+      persons: persons
+    });
   }
 
-  render() {
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState( { showPersons: !doesShow } );
+  }
+
+  render () {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if ( this.state.showPersons ) {
+      persons = (
+        <div>
+          {
+            // iterate thru the array of persons and return an array of <Person> elements
+            this.state.persons.map((person, index) => {
+              return <Person 
+                key={person.id}
+                name={person.name}
+                age={person.age}
+                click={() => this.deletePersonHandler(index)} // pass an anonymous function call
+                changed = {(event) => this.nameChangedHandler(event, person.id)}
+                />
+            })
+          }
+        </div>
+      );
+    }
+
     return (
       <div className="App">
-        <ol>
-          <li>Create TWO new components: UserInput and UserOutput</li>
-          <li>UserInput should hold an input element, UserOutput two paragraphs</li>
-          <li>Output multiple UserOutput components in the App component (any paragraph texts of your choice)</li>
-          <li>Pass a username (of your choice) to UserOutput via props and display it there</li>
-          <li>Add state to the App component (=> the username) and pass the username to the UserOutput component</li>
-          <li>Add a method to manipulate the state (=> an event-handler method)</li>
-          <li>Pass the event-handler method reference to the UserInput component and bind it to the input-change event</li>
-          <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
-          <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
-          <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
-        </ol>
-        <UserInput 
-          changed={this.usernameChangedHandler} 
-          currentName={this.state.username} />
-        <UserOutput userName={this.state.username} />
-        <UserOutput userName={this.state.username} />
-        <UserOutput userName="Max" />
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
     );
   }
